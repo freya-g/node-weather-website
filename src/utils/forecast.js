7c2603f98ad0ff6db339491c3e555ce45c35e212
Freya <fgao30@student.monash.edu>
2019-11-15 18:51:12 +1100

    Add info to forecast, edit about image

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,9 +8,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.')
         } else {
+            sunsetTime = new Date(body.daily.data[0].sunsetTime * 1000);// Convert sunset time from UNIX
             callback(
                 undefined,
-                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability*100 + '% chance of rain.'
+                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability*100 + '% chance of rain. The sun sets at ' + sunsetTime.getHours() + ":" + sunsetTime.getMinutes().toString().padStart(2, '0') + "."
                 )
         }
     })
